542cf1c5b784dd81ed1f17425ce2172bb31f9688
manisha14052008 <manisharengarajan@gmail.com>
2026-08-13 19:03:25 +0530

    Add JsonUtil class for JSON serialization and deserialization

---
+++ src/main/java/com/manisha/manishamart/util/JsonUtil.java
@@ -0,0 +1,17 @@
+package com.manisha.manishamart.util;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+public class JsonUtil {
+    private static final Gson GSON = new GsonBuilder()
+            .setDateFormat("yyyy-MM-dd'T'HH:mm:ss")
+            .create();
+    private JsonUtil() {}
+
+    public static String toJson(Object obj) {
+        return GSON.toJson(obj);
+    }
+    public static <T> T fromJson(String json, Class<T> clazz) {
+        return GSON.fromJson(json, clazz);
+    }
+}
